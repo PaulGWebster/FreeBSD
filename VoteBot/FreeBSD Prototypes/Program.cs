b0452a7fc1d33ce4bbfc2932b21d41afc6ce5c18
Paul G Webster <paul.g.webster@googlemail.com>
2018-01-27 17:06:12 +0000

    Initial Commit

---
+++ VoteBot/FreeBSD Prototypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeBSD_Prototypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
